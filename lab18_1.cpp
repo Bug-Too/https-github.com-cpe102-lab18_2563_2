c662d78976e8eee8a5e2b12fd16f8566b325a1d5
Purich Seenuallae <purichseenuallae@gmail.com>
2021-02-23 17:42:47 +0700

    finished

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,49 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect
+{
+	double x, y, w, h;
 };
+double min(double x, double y)
+{
+	if (x < y)
+	{
+		return x;
+	}
+	else
+	{
+		return y;
+	}
+}
+double max(double x, double y)
+{
+	if (x > y)
+	{
+		return x;
+	}
+	else
+	{
+		return y;
+	}
+}
 
-double overlap(){
-
-
-
+double overlap(Rect r1, Rect r2)
+{
+	double l1 = min(r1.x + r1.w, r2.x + r2.w) - max(r1.x, r2.x);
+	double l2 = min(r1.y, r2.y) - max(r1.y - r1.h, r2.y - r2.h);
+	double area;
+	area = (l1 * l2);
+	if (area<0)
+	{
+		return 0;
+	}
+	 
+	return area;
+}
+int main()
+{
+	Rect R1 = {1, 1, 5, 5};
+	Rect R2 = {7, 2, 3, 3};
+	cout << overlap(R1, R2);
 }
--- lab18_3.cpp
+++ lab18_3.cpp
@@ -10,6 +10,10 @@ using namespace std;
 struct student{
 
     //Define struct student with four member (name ,id , gender, gpa);
+	char name[100];
+	int id;
+	char gender;
+	float gpa;
     
 };
 
@@ -20,7 +24,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){ //There is something wrong in this line. change to float
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -58,19 +62,19 @@ int main(){
 	ifstream course_file("courses.txt");
 	vector<student> allstudents;
 	vector<course> allcourses;
-	
 	string textline;
-	
+	char fomat[] ="%[^,],%d,%c,%f";
 	while(getline(student_file,textline)){
 		student s; 
-		
 		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-
-		allstudents.push_back(s); 		
+		sscanf(textline.c_str(),fomat,s.name,&s.id,&s.gender,&s.gpa);
+		allstudents.push_back(s); 
 	}
+	
 	
 	int state = 1;
 	while(getline(course_file,textline)){
+		
 		if(state == 1){
 			course c;
 			int loc = textline.find_first_of('(');
@@ -82,8 +86,8 @@ int main(){
 		}else if(state == 2){
 			if(textline == "> Students"){
 				state = 3;
-			}else{
-			
+			}else{ 
+				allcourses[allcourses.size()-1].lecture_list.push_back(textline);
 			    //Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
 			    
 			}			
@@ -92,7 +96,7 @@ int main(){
 				state = 1;
 			}else{
 				student *p = findstudent(allstudents,atof(textline.c_str()));
-				
+				allcourses[allcourses.size()-1].student_list.push_back(p);
 				//Append (push_back) p to student_list of the recently added course in allcourses[];
 				
 			}
